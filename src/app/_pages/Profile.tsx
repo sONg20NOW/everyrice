dac86f16ce9dd0df0c29e969d2d6d6b1600dac12
sONg20NOW <sgk1004s@naver.com>
2025-10-19 15:22:08 +0900

    수업추가 다이얼로그 컴포넌트화, 전체 삭제 다이얼로그화

--- src/app/_pages/Profile.tsx
+++ src/app/_pages/Profile.tsx
@@ -6,14 +6,7 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Badge } from "@/components/ui/badge";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+
 import {
   Select,
   SelectContent,
@@ -38,14 +31,18 @@ import {
 } from "lucide-react";
 import { CalculateTime } from "@/lib/calculateTime";
 import { useForm } from "react-hook-form";
+import AddClassDialog from "@/components/AddClassDialog";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 interface ProfileProps {
   currentUser: User;
@@ -165,12 +162,10 @@ export default function Profile({
   };
 
   const clearTimetable = () => {
-    if (confirm("모든 시간표를 삭제하시겠습니까?")) {
-      setEditedUser({
-        ...editedUser,
-        timetable: [],
-      });
-    }
+    setEditedUser({
+      ...editedUser,
+      timetable: [],
+    });
   };
 
   return (
@@ -403,13 +398,10 @@ export default function Profile({
               {isEditing && (
                 <div className="flex space-x-2">
                   {editedUser.timetable.length > 0 && (
-                    <Button
-                      size="sm"
-                      variant="destructive"
-                      onClick={clearTimetable}
-                    >
-                      전체 삭제
-                    </Button>
+                    <div className="flex gap-2">
+                      <AddClassDialog setEditedUser={setEditedUser} />
+                      <ClearTimeTableDialog clearTimetable={clearTimetable} />
+                    </div>
                   )}
                 </div>
               )}
@@ -479,205 +471,32 @@ export default function Profile({
   );
 }
 
-function AddClassDialog({
-  setEditedUser,
+function ClearTimeTableDialog({
+  clearTimetable,
 }: {
-  setEditedUser: React.Dispatch<React.SetStateAction<User>>;
+  clearTimetable: () => void;
 }) {
-  const [open, setOpen] = useState(false);
-
-  const form = useForm<TimeSlot>({
-    defaultValues: {
-      day: 0, // 월요일
-      startTime: 9,
-      endTime: 10.5,
-      subject: "",
-      location: "",
-      professor: "",
-    },
-  });
-
-  const addClass = (data: TimeSlot) => {
-    if (data.startTime >= data.endTime) {
-      form.setError("endTime", {
-        type: "manual",
-        message: "종료 시간은 시작 시간보다 늦어야 합니다.",
-      });
-      return;
-    }
-
-    setEditedUser((prev) => ({
-      ...prev,
-      timetable: [...prev.timetable, data],
-    }));
-
-    setOpen(false);
-  };
-
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
-        <Button variant="outline">
-          <Plus className="w-4 h-4 mr-2" />
-          수업 추가
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button type="button" variant="destructive">
+          전체 삭제
         </Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>수업 추가</DialogTitle>
-        </DialogHeader>
-        <Form {...form}>
-          <form className="space-y-4" onSubmit={form.handleSubmit(addClass)}>
-            {/* 과목명 (Input) */}
-            <FormField
-              control={form.control}
-              name="subject"
-              rules={{ required: "과목명은 필수 입력입니다." }}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="subject">과목명</FormLabel>
-                  <FormControl>
-                    <Input placeholder="과목명을 입력하세요" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <div className="grid grid-cols-2 gap-4">
-              {/* 요일 (Select) */}
-              <FormField
-                control={form.control}
-                name="day"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel htmlFor="day">요일</FormLabel>
-                    <Select
-                      onValueChange={field.onChange} // field.onChange 연결
-                      defaultValue={field.value.toString()}
-                    >
-                      <FormControl>
-                        <SelectTrigger className="w-full">
-                          <SelectValue placeholder="요일 선택" />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        <SelectItem value="0">월요일</SelectItem>
-                        <SelectItem value="1">화요일</SelectItem>
-                        <SelectItem value="2">수요일</SelectItem>
-                        <SelectItem value="3">목요일</SelectItem>
-                        <SelectItem value="4">금요일</SelectItem>
-                      </SelectContent>
-                    </Select>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {/* 강의실 (Input) */}
-              <FormField
-                control={form.control}
-                name="location"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel htmlFor="location">강의실</FormLabel>
-                    <FormControl>
-                      <Input placeholder="강의실" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <div className="grid grid-cols-2 gap-4">
-              {/* 시작 시간 (Input type="number") */}
-              <FormField
-                control={form.control}
-                name="startTime"
-                rules={{
-                  required: "필수 입력입니다.",
-                  min: { value: 9, message: "최소 9시부터 가능합니다." },
-                  max: { value: 18, message: "최대 18시까지 가능합니다." },
-                }}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel htmlFor="startTime">시작 시간</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="number"
-                        step="0.5"
-                        min="9"
-                        max="18"
-                        // Input type="number"는 value를 문자열로 관리해야 RHF에서 경고가 뜨지 않습니다.
-                        // 따라서 field.value는 문자열, onChange는 e.target.value를 받습니다.
-                        {...field}
-                        onChange={(e) => {
-                          field.onChange(e.target.value);
-                        }}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              {/* 종료 시간 (Input type="number") */}
-              <FormField
-                control={form.control}
-                name="endTime"
-                rules={{
-                  required: "필수 입력입니다.",
-                  min: { value: 9, message: "최소 9시부터 가능합니다." },
-                  max: { value: 18, message: "최대 18시까지 가능합니다." },
-                }}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel htmlFor="endTime">종료 시간</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="number"
-                        step="0.5"
-                        min="9"
-                        max="18"
-                        {...field}
-                        onChange={(e) => {
-                          field.onChange(e.target.value);
-                        }}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            {/* 교수명 (Input) */}
-            <FormField
-              control={form.control}
-              name="professor"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="professor">교수명</FormLabel>
-                  <FormControl>
-                    <Input placeholder="교수명" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <div className="flex justify-end space-x-2">
-              <DialogClose asChild>
-                <Button type="button">취소</Button>
-              </DialogClose>
-              <Button
-                type="submit"
-                disabled={
-                  form.formState.isSubmitting || !form.formState.isDirty
-                }
-              >
-                추가
-              </Button>
-            </div>
-          </form>
-        </Form>
-      </DialogContent>
-    </Dialog>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>시간표 삭제</AlertDialogTitle>
+          <AlertDialogDescription>
+            모든 시간표를 삭제하시겠습니까?
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <Button type="button" onClick={clearTimetable}>
+            확인
+          </Button>
+          <AlertDialogCancel>취소</AlertDialogCancel>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
---
+++ src/components/AddClassDialog.tsx
@@ -0,0 +1,233 @@
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { TimeSlot, User } from "@/types";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "./ui/button";
+import { Plus } from "lucide-react";
+import { Input } from "./ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+
+export default function AddClassDialog({
+  setEditedUser,
+}: {
+  setEditedUser: React.Dispatch<React.SetStateAction<User>>;
+}) {
+  const [open, setOpen] = useState(false);
+
+  const form = useForm<TimeSlot>({
+    defaultValues: {
+      day: 0, // 월요일
+      startTime: 9,
+      endTime: 10.5,
+      subject: "",
+      location: "",
+      professor: "",
+    },
+  });
+
+  const addClass = (data: TimeSlot) => {
+    if (data.startTime >= data.endTime) {
+      form.setError("endTime", {
+        type: "manual",
+        message: "종료 시간은 시작 시간보다 늦어야 합니다.",
+      });
+      return;
+    }
+
+    setEditedUser((prev) => ({
+      ...prev,
+      timetable: [...prev.timetable, data],
+    }));
+
+    setOpen(false);
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant="outline">
+          <Plus className="w-4 h-4 mr-2" />
+          수업 추가
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>수업 추가</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form className="space-y-4" onSubmit={form.handleSubmit(addClass)}>
+            {/* 과목명 (Input) */}
+            <FormField
+              control={form.control}
+              name="subject"
+              rules={{ required: "과목명은 필수 입력입니다." }}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel htmlFor="subject">과목명</FormLabel>
+                  <FormControl>
+                    <Input placeholder="과목명을 입력하세요" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="grid grid-cols-2 gap-4">
+              {/* 요일 (Select) */}
+              <FormField
+                control={form.control}
+                name="day"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel htmlFor="day">요일</FormLabel>
+                    <Select
+                      onValueChange={field.onChange} // field.onChange 연결
+                      defaultValue={field.value.toString()}
+                    >
+                      <FormControl>
+                        <SelectTrigger className="w-full">
+                          <SelectValue placeholder="요일 선택" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="0">월요일</SelectItem>
+                        <SelectItem value="1">화요일</SelectItem>
+                        <SelectItem value="2">수요일</SelectItem>
+                        <SelectItem value="3">목요일</SelectItem>
+                        <SelectItem value="4">금요일</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* 강의실 (Input) */}
+              <FormField
+                control={form.control}
+                name="location"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel htmlFor="location">강의실</FormLabel>
+                    <FormControl>
+                      <Input placeholder="강의실" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              {/* 시작 시간 (Input type="number") */}
+              <FormField
+                control={form.control}
+                name="startTime"
+                rules={{
+                  required: "필수 입력입니다.",
+                  min: { value: 9, message: "최소 9시부터 가능합니다." },
+                  max: { value: 18, message: "최대 18시까지 가능합니다." },
+                }}
+                render={({ field }) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel htmlFor="startTime">시작 시간</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="number"
+                        step="0.5"
+                        min="9"
+                        max="18"
+                        // Input type="number"는 value를 문자열로 관리해야 RHF에서 경고가 뜨지 않습니다.
+                        // 따라서 field.value는 문자열, onChange는 e.target.value를 받습니다.
+                        {...field}
+                        onChange={(e) => {
+                          field.onChange(Number(e.target.value));
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {/* 종료 시간 (Input type="number") */}
+              <FormField
+                control={form.control}
+                name="endTime"
+                rules={{
+                  required: "필수 입력입니다.",
+                  min: { value: 9, message: "최소 9시부터 가능합니다." },
+                  max: { value: 18, message: "최대 18시까지 가능합니다." },
+                }}
+                render={({ field }) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel htmlFor="endTime">종료 시간</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="number"
+                        step="0.5"
+                        min="9"
+                        max="18"
+                        {...field}
+                        onChange={(e) => {
+                          field.onChange(Number(e.target.value));
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            {/* 교수명 (Input) */}
+            <FormField
+              control={form.control}
+              name="professor"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel htmlFor="professor">교수명</FormLabel>
+                  <FormControl>
+                    <Input placeholder="교수명" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex justify-end space-x-2">
+              <DialogClose asChild>
+                <Button type="button">취소</Button>
+              </DialogClose>
+              <Button
+                type="submit"
+                disabled={
+                  form.formState.isSubmitting || !form.formState.isDirty
+                }
+              >
+                추가
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
